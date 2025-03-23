366b8660c67c77441fe076ee837feef48c29581e
Tung Dao <daotung492@gmail.com>
2025-03-23 23:59:45 +0700

    u

---
+++ Services/IdentityService/Entities/ApplicationUser.cs
@@ -0,0 +1,11 @@
+﻿using AspNetCore.Identity.MongoDbCore.Models;
+using MongoDbGenericRepository.Attributes;
+
+namespace IdentityService.Entities
+{
+    [CollectionName("Users")]
+    public class ApplicationUser : MongoIdentityUser<Guid>
+    {
+        public decimal Gil { get; set; }
+    }
+}
