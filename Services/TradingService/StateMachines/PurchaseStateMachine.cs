f13af799b0ca81392800c8203b2d372ef0d0a299
Tung Dao <daotung492@gmail.com>
2025-04-14 16:41:53 +0700

    add compensation transaction

--- Services/TradingService/StateMachines/PurchaseStateMachine.cs
+++ Services/TradingService/StateMachines/PurchaseStateMachine.cs
@@ -1,5 +1,6 @@
 ﻿using Automatonymous;
 using BuildingBlocks.Common.Contracts;
+using MassTransit;
 using TradingService.Activities;
 
 namespace TradingService.StateMachines
@@ -14,6 +15,8 @@ namespace TradingService.StateMachines
         public Event<GetPurchaseState>? GetPurchaseState { get; }
         public Event<InventoryItemsGranted>? InventoryItemsGranted { get; }
         public Event<GilDebited>? GilDebited { get; }
+        public Event<Fault<GrantItems>>? GrantItemsFaulted { get; }
+        public Event<Fault<DebitGil>>? DebitGilFaulted { get; }
 
         public PurchaseStateMachine()
         {
@@ -23,6 +26,7 @@ namespace TradingService.StateMachines
             ConfigureAny();
             ConfigureAccepted();
             ConfigItemsGranted();
+            ConfigFaulted();
         }
 
         public void ConfigureEvents()
@@ -31,6 +35,8 @@ namespace TradingService.StateMachines
             Event(() => GetPurchaseState);
             Event(() => InventoryItemsGranted);
             Event(() => GilDebited);
+            Event(() => GrantItemsFaulted, x => x.CorrelateById(context => context.Message.Message.CorrelationId));
+            Event(() => DebitGilFaulted, x => x.CorrelateById(context => context.Message.Message.CorrelationId));
         }
 
         public void ConfigureInitialState()
@@ -81,7 +87,14 @@ namespace TradingService.StateMachines
                         context.Instance.UserId,
                         context.Instance.PurchaseTotal,
                         context.Instance.CorrelationId))
-                    .TransitionTo(ItemsGranted)
+                    .TransitionTo(ItemsGranted),
+                When(GrantItemsFaulted)
+                    .Then(context =>
+                    {
+                        context.Instance.ErrorMessage = context.Data.Exceptions.First().Message;
+                        context.Instance.LastUpdated = DateTimeOffset.UtcNow;
+                    })
+                    .TransitionTo(Faulted)
             );
         }
 
@@ -89,11 +102,33 @@ namespace TradingService.StateMachines
         {
             During(ItemsGranted,
                 When(GilDebited)
-                .Then(context =>
-                {
-                    context.Instance.LastUpdated = DateTimeOffset.UtcNow;
-                })
-                .TransitionTo(Completed));
+                    .Then(context =>
+                    {
+                        context.Instance.LastUpdated = DateTimeOffset.UtcNow;
+                    })
+               .TransitionTo(Completed),
+                When(DebitGilFaulted)
+                    .Send(context => new SubtractItems(
+                        context.Instance.UserId,
+                        context.Instance.ItemId,
+                        context.Instance.Quantity,
+                        context.Instance.CorrelationId))
+                    .Then(context =>
+                    {
+                        context.Instance.ErrorMessage = context.Data.Exceptions.First().Message;
+                        context.Instance.LastUpdated = DateTimeOffset.UtcNow;
+                    })
+                    .TransitionTo(Faulted)
+                );
+        }
+
+        private void ConfigFaulted()
+        {
+            During(Faulted,
+                Ignore(PurchaseRequested),
+                Ignore(InventoryItemsGranted),
+                Ignore(GilDebited)
+            );
         }
     }
 }
